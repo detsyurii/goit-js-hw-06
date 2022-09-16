f048a8a7946e269febdfb3bb8dfb1b214f19daad
detsyurii <dets.yurii@gmail.com>
2022-09-16 13:37:09 +0300

    fix mistake

--- js/task-01.js
+++ js/task-01.js
@@ -2,6 +2,6 @@ const itemsRef = document.querySelectorAll('li.item')
 console.log( 'Number of categories:', itemsRef.length);
 
 itemsRef.forEach(item => {
-    console.log('Category:', item.querySelector('h2').textContent);
-    console.log('Elements:', item.querySelectorAll('li').length);
+    console.log('Category:', item.firstElementChild.textContent);
+    console.log('Elements:', item.lastElementChild.children.length);
 })
--- js/task-06.js
+++ js/task-06.js
@@ -3,11 +3,12 @@ const inputRef = document.querySelector('#validation-input');
 
 
 const onInputBlur = event => {
-	if (event.currentTarget.value.length <= inputRef.dataset.length) {
-		inputRef.classList.add('invalid');
-	} else {
+	if (event.currentTarget.value.length === Number(inputRef.dataset.length)) {
 		inputRef.classList.add('valid');
 		inputRef.classList.remove('invalid');
+	} else {
+		inputRef.classList.add('invalid');
+		classList.remove('valid');
 	}
 };
 
