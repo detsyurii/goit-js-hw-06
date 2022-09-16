668f08c900e7e162175be11031091027e95c88b0
detsyurii <dets.yurii@gmail.com>
2022-09-16 14:42:49 +0300

    Update task-07.js

--- js/task-07.js
+++ js/task-07.js
@@ -1,5 +1,6 @@
 const inputRangeRef = document.querySelector('#font-size-control');
-const textRangeRef = document.querySelector('#text');
+const textRangeRef = document.querySelector('span');
+textRangeRef.style.fontSize = "56px";
 
 const handlerText = (event) => {
     textRangeRef.style.fontSize =`${event.target.value}px`;
