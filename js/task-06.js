ba73818fd3aa0bec19edce932c74cfc314a7256f
detsyurii <dets.yurii@gmail.com>
2022-10-12 09:51:37 +0300

    Update task-06.js

--- js/task-06.js
+++ js/task-06.js
@@ -1,17 +1,13 @@
+const inputRef = document.querySelector("#validation-input");
 
-const inputRef = document.querySelector('#validation-input');
-
-
-const onInputBlur = event => {
-	if (event.currentTarget.value.length === Number(inputRef.dataset.length)) {
-		inputRef.classList.add('valid');
-		inputRef.classList.remove('invalid');
-	} else {
-		inputRef.classList.add('invalid');
-		classList.remove('valid');
-	}
+const onInputBlur = (event) => {
+  if (event.currentTarget.value.length === Number(inputRef.dataset.length)) {
+    inputRef.classList.add("valid");
+    inputRef.classList.remove("invalid");
+  } else {
+    inputRef.classList.add("invalid");
+    inputRef.classList.remove("valid");
+  }
 };
 
-inputRef.addEventListener('blur', onInputBlur);
-
-
+inputRef.addEventListener("blur", onInputBlur);
